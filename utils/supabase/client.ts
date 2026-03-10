6f2c3281930facb490afdf3e7babbf22d0bf5e29
maestriasespe <maestriasespe@gmail.com>
2026-03-10 12:17:43 -0600

    Fix Supabase client alias

---
+++ utils/supabase/client.ts
@@ -0,0 +1,8 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  return createBrowserClient(
+    "https://ftnbkfeqkxcnclsazjpc.supabase.co",
+    "sb_publishable_fabv1L2fQuDfJpxPMSDhlQ_F6FDBv3H"
+  );
+}
--- utils/supabase/client.ts.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-export const supabase = createBrowserClient(
-  "https://ftnbkfeqkxcnclsazjpc.supabase.co",
-  "sb_publishable_fabv1L2fQuDfJpxPMSDhlQ_F6FDBv3H"
-);
